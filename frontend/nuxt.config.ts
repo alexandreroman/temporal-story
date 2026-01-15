b774076ae85f48ffec7ba7c7e6b423109dffe611
Alexandre Roman <alexandre.roman@gmail.com>
2026-01-15 02:56:05 +0100

    Fix SPA routing

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
 
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   app: {
